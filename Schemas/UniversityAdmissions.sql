ad44b83f7c4c451fbab2542850f263bbecdb00a4
kgueorgu <kgueorgu@ryerson.ca>
2021-12-01 15:51:33 -0500

    update UniversityAdmissions

--- Schemas/UniversityAdmissions.sql
+++ Schemas/UniversityAdmissions.sql
@@ -4,10 +4,13 @@ drop table if exists Apply;
 
 /* Create the schema for our tables */
 create table College(cName text, state text, enrollment int);
-create table Student(sID int, cName text, major text, decision text);
+create table Student(sID int, sName text, GPA float, sizeHS int);
 create table Apply(sID int, cName text, major text, decision text);
-
 /* Populate the tables with our data */
+insert into College values('Stanford', 'CA', 15000);
+insert into College values('Berkeley', 'CA', 36000);
+insert into College values('MIT', 'MA', 10000);
+insert into College values('Cornell', 'NY', 21000);
 insert into Apply values (123, 'Stanford', 'CS', 'Y');
 insert into Apply values (123, 'Stanford', 'EE', 'N');
 insert into Apply values (123, 'Berkley', 'CS', 'Y');
@@ -27,4 +30,16 @@ insert into Apply values (765, 'Stanford', 'history', 'Y');
 insert into Apply values (765, 'Cornell', 'history', 'N');
 insert into Apply values (765, 'Cornell', 'psychology', 'Y');
 insert into Apply values (543, 'MIT', 'CS', 'N');
+insert into Student values(123, 'Amy', 3.9 , 1000);
+insert into Student values(234, 'Bob', 3.6 , 1500);
+insert into Student values(345, 'Craig', 3.5 , 500);
+insert into Student values(456, 'Doris', 3.9 , 1000);
+insert into Student values(567, 'Edward', 2.9 , 2000);
+insert into Student values(678, 'Fay', 3.8 , 200);
+insert into Student values(789, 'Gary', 3.4 , 800);
+insert into Student values(987, 'Helen', 3.7 , 800);
+insert into Student values(876, 'Irene', 3.9 , 400);
+insert into Student values(765, 'Jay', 2.9 , 1500);
+insert into Student values(654, 'Amy', 3.9 , 1000);
+insert into Student values(543, 'Craig', 3.4 , 2000);
 
