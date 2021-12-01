786a0d4be4a4767e5c2cc9672bf7ed833dbf8412
pav <pavel.gueorguiev@gmail.com>
2021-12-01 21:48:40 +0100

    [add]  Apply values in uni

--- Schemas/UniversityAdmissions.sql
+++ Schemas/UniversityAdmissions.sql
@@ -1,11 +1,30 @@
 /* Delete the tables if they already exist */
 drop table if exists College;
+drop table if exists Apply;
 
-
+/* Create the schema for our tables */
 create table College(cName text, state text, enrollment int);
 create table Student(sID int, cName text, major text, decision text);
+create table Apply(sID int, cName text, major text, decision text);
 
-
-
-
+/* Populate the tables with our data */
+insert into Apply values (123, 'Stanford', 'CS', 'Y');
+insert into Apply values (123, 'Stanford', 'EE', 'N');
+insert into Apply values (123, 'Berkley', 'CS', 'Y');
+insert into Apply values (123, 'Cornell', 'EE', 'Y');
+insert into Apply values (234, 'Berkley', 'biology', 'N');
+insert into Apply values (345, 'MIT', 'bioengineering', 'Y');
+insert into Apply values (345, 'Cornell', 'bioengineering', 'N');
+insert into Apply values (345, 'Cornell', 'CS', 'Y');
+insert into Apply values (345, 'Cornell', 'EE', 'N');
+insert into Apply values (678, 'Stanford', 'history', 'Y');
+insert into Apply values (987, 'Stanford', 'CS', 'Y');
+insert into Apply values (987, 'Berkeley', 'CS', 'Y');
+insert into Apply values (876, 'Stanford', 'CS', 'N');
+insert into Apply values (876, 'MIT', 'biology', 'Y');
+insert into Apply values (876, 'MIT', 'marine biology', 'N');
+insert into Apply values (765, 'Stanford', 'history', 'Y');
+insert into Apply values (765, 'Cornell', 'history', 'N');
+insert into Apply values (765, 'Cornell', 'psychology', 'Y');
+insert into Apply values (543, 'MIT', 'CS', 'N');
 
